42e4ab2379b0a1f9854c4ed8cb5166328e005533
HugoPereira97 <dhugo045@proton.me>
2025-11-27 12:31:42 +0000

    fix(models): update Order model to include product and product quantities

--- app/src/main/java/ipca/project/lojasas/models/Order.kt
+++ app/src/main/java/ipca/project/lojasas/models/Order.kt
@@ -2,9 +2,15 @@ package ipca.project.lojasas.models
 
 import java.util.Date
 
+data class OrderItem(
+    val name: String? = null,
+    val quantity: Int? = 0,
+)
+
 data class Order (
     var docId : String? = null,
     var orderDate : Date? = null,
     var surveyDate : Date? = null,
-    var accept : Boolean = false
+    var accept : Boolean = false,
+    var items: MutableList<OrderItem> = mutableListOf() // List of items and quantities
 )
